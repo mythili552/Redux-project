f85a464c26b3f6824e82f85462ad36a1c4de3eff
mythili552 <mythili@axesstechnology.in>
2026-02-04 14:47:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "./usersSlice";
+import {useEffect} from "react";
+
+function App() {
+  const dispatch = useDispatch();
+  const { users, loading, error } = useSelector((state) => state.users);
+
+  useEffect(()=>{
+    dispatch(fetchUsers());
+
+  },[dispatch]);
+
+
+  if (loading)
+    return <h2>loading....</h2>
+  if (error)
+    return <h2>{error}</h2>
+
+  return (
+    <>
+      <h1>Users:</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+    </>
+  )
+}
+export default App;
